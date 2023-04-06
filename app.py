316c93bb242b6618658558d428dd405d0a90c2ac
PriyankaGogineni <124533146+PriyankaGogineni@users.noreply.github.com>
2023-04-06 19:40:25 -0400

    Webapp-Flask

---
+++ app.py
@@ -0,0 +1,25 @@
+import re
+from datetime import datetime
+from flask import render_template
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+     return render_template("home.html")
+    
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
+
+    
