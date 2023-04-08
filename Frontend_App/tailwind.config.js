be9fd3d0e5c70aca03938faa353c83212a099a75
sohamroy <soham.roy@gmail.com>
2023-04-08 17:18:37 -0400

    Add files via upload

---
+++ Frontend_App/tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#60a5fa',
+      'green': '#4ade80',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'white': '#fff',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+
