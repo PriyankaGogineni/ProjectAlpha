f2530556ac6074b513015414459b3ed18ff27c0c
Harish Datta <60423913+Harish790@users.noreply.github.com>
2023-04-09 10:01:21 -0400

    Update Form1.Designer.cs

--- trash data/Form1.Designer.cs
+++ trash data/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace trash_data
+namespace trash_data
 {
     partial class Form1
     {
@@ -33,6 +33,15 @@
             this.product_tb = new System.Windows.Forms.TextBox();
             this.submit_btn = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -61,11 +70,12 @@
             this.product_tb.Name = "product_tb";
             this.product_tb.Size = new System.Drawing.Size(125, 27);
             this.product_tb.TabIndex = 2;
+            this.product_tb.TextChanged += new System.EventHandler(this.product_tb_TextChanged);
             // 
             // submit_btn
             // 
             this.submit_btn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.submit_btn.Location = new System.Drawing.Point(134, 207);
+            this.submit_btn.Location = new System.Drawing.Point(133, 176);
             this.submit_btn.Name = "submit_btn";
             this.submit_btn.Size = new System.Drawing.Size(113, 36);
             this.submit_btn.TabIndex = 3;
@@ -82,11 +92,102 @@
             this.label3.Size = new System.Drawing.Size(0, 28);
             this.label3.TabIndex = 4;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(159, 245);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 28);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Daily Status";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 312);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Plastic";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(60, 349);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Paper";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(60, 399);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 20);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Glass";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(62, 450);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(84, 20);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Card board";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(230, 312);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(17, 20);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "2";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(230, 351);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(17, 20);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "3";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(230, 399);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(17, 20);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "0";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(230, 450);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(17, 20);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "5";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 595);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.submit_btn);
             this.Controls.Add(this.product_tb);
@@ -106,5 +207,14 @@
         private TextBox product_tb;
         private Button submit_btn;
         private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
     }
 }
